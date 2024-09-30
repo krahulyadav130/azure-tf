9ca11d2c52371b50e1b483921eb4c602873e5ede
Jasmin Shrestha <jasmin.shrestha@plantura.garden>
2024-09-30 17:24:10 +0545

    feat: module

--- compute/rsg1.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "dev-rsg" {
-    name = "rahul-dev"
-    location = "CentralIndia"
-  
-}
---
+++ modules/compute/rsg.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rsg-name" {
+    name = var.rsgname
+    location = var.location
+  
+}
